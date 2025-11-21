f4872f0652a088c4cf3d732734de337ed4ac85c7
Cheney98004 <cheney98004@gmail.com>
2025-11-22 07:32:24 +0800

    ver 0.0.0 (formal)

--- build/CMakeFiles/boot_bin.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/boot_bin"
-  "boot.bin"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/boot_bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/kernel_obj.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for kernel_obj.
-# This may be replaced when dependencies are built.
